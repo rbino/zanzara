9bdee850f626171ae2a4659297d770e2ceda0f30
Riccardo Binetti <rbino@gmx.com>
2022-07-11 01:02:31 +0200

    Add example and repopulate README
    Signed-off-by: Riccardo Binetti <rbino@gmx.com>

--- README.md
+++ README.md
@@ -3,4 +3,28 @@
     <a href="LICENSE"><img src="https://badgen.net/github/license/rbino/zanzara" /></a>
 </p>
 
-An I/O agnostic MQTT client written in Zig.
+An allocation-free, I/O-agnostic MQTT client written in Zig.
+
+_Warning: this is a work in progress, the API is subject to change_
+
+## Features
+
+- [x] MQTT 3.1.1 client packet serialization/deserialization
+- [x] Ping handling
+- [x] Subscribe and receive data
+- [ ] Publish
+- [ ] Everything else (including a more detailed list of what's missing)
+
+## Example
+
+You can run the example with:
+
+```
+zig run example.zig
+```
+
+You can use `mosquitto_pub` to publish to the topic to verify the client is subscribed
+
+```
+mosquitto_pub -h "test.mosquitto.org" -t "zig/zanzara" -m "Hello, MQTT"
+```
---
+++ example.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const net = std.net;
+const os = std.os;
+const zanzara = @import("src/zanzara.zig");
+const Client = zanzara.mqtt4.Client;
+const Subscribe = zanzara.mqtt4.packet.Subscribe;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const stream = try net.tcpConnectToHost(allocator, "test.mosquitto.org", 1883);
+    const socket = stream.handle;
+    const writer = stream.writer();
+
+    var mqtt_buf: [2048]u8 = undefined;
+
+    var client = try Client.init(mqtt_buf[0..1024], mqtt_buf[1024..]);
+    try client.connect(.{ .client_id = "zanzara", .keepalive = 5 });
+
+    var read_buf: [2048]u8 = undefined;
+
+    while (true) {
+        // We use os.MSG.DONTWAIT so the socket returns WouldBlock if no data is present
+        const bytes = os.recv(socket, &read_buf, os.MSG.DONTWAIT) catch |err|
+            if (err == error.WouldBlock) 0 else return err;
+        var rest = read_buf[0..bytes];
+        while (true) {
+            // The driving force of the client is the client.feed() function
+            // This must be called periodically, either passing some data coming from the network
+            // or with an empty slice (if no incoming data is present) to allow the client to handle
+            // its periodic tasks, like pings etc.
+            const event = client.feed(rest);
+            switch (event.data) {
+                .incoming_packet => |p| {
+                    switch (p) {
+                        .connack => {
+                            std.debug.print("Connected, sending subscriptions\n", .{});
+                            // Subscribe to the topic we're publishing on
+                            const topics = [_]Subscribe.Topic{
+                                .{ .topic_filter = "zig/zanzara", .qos = .qos0 },
+                            };
+
+                            try client.subscribe(&topics);
+                        },
+                        .publish => |pb| {
+                            std.debug.print("Received publish on topic {s} with payload {s}\n", .{ pb.topic, pb.payload });
+                        },
+                        else => std.debug.print("Received packet: {}\n", .{p}),
+                    }
+                },
+                .outgoing_buf => |b| try writer.writeAll(b), // Write pending stuff to the socket
+                .err => |e| std.debug.print("Error event: {}", .{e}),
+                .none => {},
+            }
+            rest = rest[event.consumed..];
+            if (rest.len == 0) break;
+        }
+    }
+}
